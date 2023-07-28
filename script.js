a22c81004be950c7601733dc45d8e6982d725883
WanomiR <wanomir@yandex.ru>
2023-07-28 08:40:06 +0300

    feat: add like button functionality

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let page = document.querySelector(".page");
-
 // open edit profile popup
-let profileEditButton = page.querySelector(".profile__edit-button");
+const profileEditButton = document.querySelector(".profile__edit-button");
 profileEditButton.addEventListener("click", () => {
-  page.querySelector(".popup").classList.add("popup_opened")
+  document.querySelector(".popup").classList.add("popup_opened")
 });
 
 // close edit profile popup
-let profileEditCloseButton = page.querySelector(".popup__close-button");
+const profileEditCloseButton = document.querySelector(".popup__close-button");
 profileEditCloseButton.addEventListener("click", () => {
-  page.querySelector(".popup").classList.remove("popup_opened");
+  document.querySelector(".popup").classList.remove("popup_opened");
 });
+
+// like button
+const likeButton = document.querySelector(".card__like-button");
+likeButton.addEventListener("click", () => {
+  likeButton.classList.toggle("card__like-button_active");
+}); // works only for the first card :(
