54632ef06124da4c3421ed76c9646f41c51ca466
WanomiR <wanomir@yandex.ru>
2023-08-02 04:38:04 +0300

    feat: profile edit form submit handler

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// ------------------------------// 
+// ----------------------------- // 
 
+// -------- Form --------------- // 
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__item_el_name");
+const jobInput = document.querySelector(".popup__item_el_description");
+
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
+
+
+// ---------- Cards ------------ // 
 const cardsContainer = document.querySelector(".places__grid");
 
 function addCard(placeName, imageLink) {
@@ -16,22 +35,7 @@ function addCard(placeName, imageLink) {
   cardsContainer.appendChild(card);
 }
 
-// ------------------------------// 
-
-// open edit profile popup
-const profileEditButton = document.querySelector(".profile__edit-button");
-profileEditButton.addEventListener("click", () => {
-  document.querySelector(".popup").classList.add("popup_opened")
-});
-
-// close edit profile popup
-const profileEditCloseButton = document.querySelector(".popup__close-button");
-profileEditCloseButton.addEventListener("click", () => {
-  document.querySelector(".popup").classList.remove("popup_opened");
-});
-
-
-// loading initial cards
+// initial set of cards
 const initialCards = [
   {
     name: 'Архыз',
@@ -59,7 +63,24 @@ const initialCards = [
   }
   ];
 
+// load the initial set of cards
 initialCards.forEach(item => {
   console.log(item.name);
   addCard(item.name, item.link);
 });
+
+// ---------- Profile ---------- // 
+
+// open edit profile popup
+const profileEditButton = document.querySelector(".profile__edit-button");
+profileEditButton.addEventListener("click", () => {
+  document.querySelector(".popup").classList.add("popup_opened")
+});
+
+// close edit profile popup
+const profileEditCloseButton = document.querySelector(".popup__close-button");
+profileEditCloseButton.addEventListener("click", () => {
+  document.querySelector(".popup").classList.remove("popup_opened");
+});
+
+
