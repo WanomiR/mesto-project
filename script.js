86e9e7dfe10d183411ced86757da878a552b194b
WanomiR <wanomir@yandex.ru>
2023-08-04 18:58:43 +0300

    refactor: savaed initial set of cards as a separate object

--- script.js
+++ script.js
@@ -146,40 +146,6 @@ profileFormElement.addEventListener("submit", submitFormProfile);
 
 // ---------- Initial set of cards ---------- //
 
-// initial set of cards
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'фотография склонов гор покрытых зеленью и снегом'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'фотография реки с заснеженными берегами'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: 'вечерняя фотография панельных домов жилого райна, в окнах горит теплый свет'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'фотография подножья горы с редкой растительностью'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: 'фотография леса с железной дорогой, уходящей в горизонт по прямой линии'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'фотография скалистых склонов на побережье озера'
-  }
-  ];
-
 // load the initial set of cards
 initialCards.forEach(item => {
   cardsContainer.prepend(createCard(item));
