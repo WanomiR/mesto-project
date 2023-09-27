7ca1945cb568cdebec93f5224711489e801f6352
Anira Uoren <airin6666@yandex.ru>
2023-09-27 22:21:12 +0300

    feat: Добавлен класс PopupWithImage
    Расширенный класс для работы с попапом, предназначенным для просмотра изображений.

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,36 @@
+import {Popup} from "./Popup";
+
+/**
+ * Обработка попапа для просмотра изображений.
+ */
+export class PopupWithImage extends Popup{
+    _imageElement;
+    _descriptionElement;
+
+    /**
+     * Создать попап.
+     * @param popupSelector {String} - селектор попапа.
+     */
+    constructor(popupSelector) {
+        super(popupSelector);
+        this._imageElement = this._popupElement.querySelector(".popup__image");
+        this._descriptionElement = this._popupElement.querySelector(".popup__image-description");
+    }
+
+    /**
+     * Прокинуть значения в попап и открыть его.
+     * @param placeName {String} - название места.
+     * @param imageLink {String} - ссылка на изображение места.
+     */
+    open(placeName, imageLink) {
+        new Promise((resolve, reject) => {
+            this._descriptionElement.textContent = placeName;
+            this._imageElement.src = imageLink;
+            this._imageElement.alt = `Фото: ${placeName}`
+            this._imageElement.onload = resolve;
+            this._imageElement.onerror = reject;
+        })
+            .then(() => super.open())
+            .catch(err => console.log(`Error opening popup image: ${err}`))
+    }
+}
