a12bb8fa1e0301f1d9888d661b5e76621b8966f3
WanomiR <wanomir@yandex.ru>
2023-09-29 17:56:24 +0300

    add UserInfo class

---
+++ src/components/UserInfo.js
@@ -0,0 +1,51 @@
+export default class UserInfo {
+    constructor({nameSelector, aboutSelector, avatarSelector}, api) {
+        this._nameElement = document.querySelector(nameSelector);
+        this._aboutElement = document.querySelector(aboutSelector);
+        this._avatarElement = document.querySelector(avatarSelector);
+        this._api = api;
+    }
+
+    loadProfileData() {
+        this._api.getUserInfo()
+            .then(res => {
+                this._nameElement.textContent = res.name;
+                this._aboutElement.textContent = res.about;
+                this._avatarElement.src = res.avatar;
+            })
+            .catch(err => console.log(err));
+    }
+
+    getUserInfo() {
+        return this._api.getUserInfo()
+            .then(res => res)
+            .catch(err => console.log(err))
+    }
+
+    setUserInfo(userData, popup) {
+        popup.renderLoading(true)
+        this._api.patchUserInfo(userData)
+            .then(res => {
+                this._nameElement.textContent = res.name;
+                this._aboutElement.textContent = res.about;
+            })
+            .catch(err => console.log(err))
+            .finally(() => {
+                popup.renderLoading(false)
+                popup.close()
+            })
+    }
+
+    setUserAvatar(imageLink, popup) {
+        popup.renderLoading(true)
+        this._api.patchUserAvatar(imageLink)
+            .then(res => {
+                this._avatarElement.src = res.avatar;
+            })
+            .catch(err => console.log(err))
+            .finally(() => {
+                popup.renderLoading(false);
+                popup.close();
+        })
+    }
+}
